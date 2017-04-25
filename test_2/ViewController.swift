7c3ce2ad67d38043c2a4109e46be7159d5531c00
wwrp1 <rokitianskiy.a@gmail.com>
2017-04-25 23:01:44 +0400

    New Feature

--- test_2/ViewController.swift
+++ test_2/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Feature_1")
+        
+        print("New Feature")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
